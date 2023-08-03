22871a70060f11919cfb008d03bb82b46010199c
Chris Shepherd <chris@chrisshepherd.me>
2023-08-03 14:18:11 +0100

    Remove getScale function

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,15 +1,3 @@
-import type { Vector2 } from "./core/vector2";
-import { createVector2 } from "./core/vector2";
-
-export const getScale = (ctx: CanvasRenderingContext2D): Vector2 => {
-	const matrix = ctx.getTransform();
-
-	const scaleX = matrix.a;
-	const scaleY = matrix.d;
-
-	return createVector2(scaleX, scaleY);
-};
-
 export const setupContext = (
 	ctx: CanvasRenderingContext2D,
 	devicePixelRatio: number,
