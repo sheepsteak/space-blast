9ad308c65577b1b8680e8fb73bef608bcf9d6662
Chris Shepherd <chris@chrisshepherd.me>
2023-09-07 12:48:35 +0100

    Create Weapon component

---
+++ src/components/weapon.ts
@@ -0,0 +1,15 @@
+import type { Component } from "../ecs/component";
+
+export const WeaponType = "WEAPON";
+
+export type Weapon = {
+	fireRate: number;
+	lastFired: number;
+	type: typeof WeaponType;
+} & Component;
+
+export const createWeapon = (fireRate: number): Weapon => ({
+	fireRate,
+	lastFired: 0,
+	type: WeaponType,
+});
