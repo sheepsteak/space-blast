7bd0c13c735a3b54e550fd241648c0022fc9375c
Chris Shepherd <chris@chrisshepherd.me>
2023-09-08 14:08:58 +0100

    Add PlayerBullet component

---
+++ src/components/player-bullet.ts
@@ -0,0 +1,11 @@
+import type { Component } from "../ecs/component";
+
+export const PlayerBulletType = "PLAYERBULLET";
+
+export type PlayerBullet = {
+	type: typeof PlayerBulletType;
+} & Component;
+
+export const createPlayerBullet = (): PlayerBullet => ({
+	type: PlayerBulletType,
+});
