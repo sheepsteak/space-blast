9b4d67dc4f774c7e60b2141211501916fe026ba2
Chris Shepherd <chris@chrisshepherd.me>
2023-09-08 14:09:55 +0100

    Add createPlayerBullet function

--- src/states/play/entities.ts
+++ src/states/play/entities.ts
@@ -2,6 +2,7 @@ import { createAcceleration } from "../../components/acceleration";
 import { createFriction } from "../../components/friction";
 import { createInput } from "../../components/input";
 import { createMaxSpeed } from "../../components/max-speed";
+import { createPlayerBullet } from "../../components/player-bullet";
 import { createPosition } from "../../components/position";
 import { createRotation } from "../../components/rotation";
 import { createSprite } from "../../components/sprite";
@@ -12,6 +13,34 @@ import { addEntityComponent } from "../../ecs/entity";
 import type { World } from "../../ecs/world";
 import { createEntity } from "../../ecs/world";
 import { toRadians } from "../../math";
+
+export interface CreateBulletArgs {
+	dx: number;
+	dy: number;
+	rotation: number;
+	world: World;
+	x: number;
+	y: number;
+}
+
+export const createPlayerBulletEntity = ({
+	dx,
+	dy,
+	world,
+	x,
+	y,
+}: CreateBulletArgs): Entity => {
+	const bullet = createEntity(world);
+	addEntityComponent(bullet, createAcceleration());
+	addEntityComponent(bullet, createFriction(1));
+	addEntityComponent(bullet, createPlayerBullet());
+	addEntityComponent(bullet, createPosition({ x, y }));
+	addEntityComponent(bullet, createRotation(toRadians(-90)));
+	addEntityComponent(bullet, createSprite("bullet"));
+	addEntityComponent(bullet, createVelocity({ x: dx, y: dy }));
+
+	return bullet;
+};
 
 export interface CreatePlayerArgs {
 	rotation: number;
