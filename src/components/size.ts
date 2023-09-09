c5e9574f38eaf2bf4d2670df10e5ec3f92b0cf3d
Chris Shepherd <chris@chrisshepherd.me>
2023-09-09 14:36:19 +0100

    Add Size component

---
+++ src/components/size.ts
@@ -0,0 +1,14 @@
+import type { Vector2 } from "../core/vector2";
+import type { Component } from "../ecs/component";
+
+export const SizeType = "SIZE";
+
+export type Size = {
+	type: typeof SizeType;
+	value: Vector2;
+} & Component;
+
+export const createSize = (value: Vector2): Size => ({
+	type: SizeType,
+	value,
+});
