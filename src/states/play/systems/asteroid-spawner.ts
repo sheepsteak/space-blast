9fbaf818737cadbb77f41673b2912f88bbad9640
Chris Shepherd <chris@chrisshepherd.me>
2023-09-14 22:55:22 +0100

    Add Asteroid Spawner System

---
+++ src/states/play/systems/asteroid-spawner.ts
@@ -0,0 +1,46 @@
+import type { Game } from "../../../components/game";
+import { GameType } from "../../../components/game";
+import { GAME_HEIGHT, GAME_WIDTH } from "../../../contants";
+import { getEntityComponent, hasEntityComponents } from "../../../ecs/entity";
+import type { System } from "../../../ecs/system";
+import { subscribe, type World } from "../../../ecs/world";
+import { createAsteroidEntity } from "../entities";
+import { LevelStart } from "../events";
+
+export type CreateAsteroidSpawnerSystemArgs = {
+	world: World;
+};
+
+export const createAsteroidSpawnerSystem = ({
+	world,
+}: CreateAsteroidSpawnerSystemArgs): System => {
+	const handleLevelStart = () => {
+		const gameEntity = world.entities.find((entity) =>
+			hasEntityComponents(entity, GameType),
+		);
+
+		if (!gameEntity) {
+			return;
+		}
+
+		const game = getEntityComponent<Game>(gameEntity, GameType);
+
+		for (let i = 0; i < game.level + 3; i++) {
+			createAsteroidEntity({
+				rotation: Math.random() * (Math.PI * 4),
+				type: "large",
+				world,
+				x: Math.random() * GAME_WIDTH,
+				y: Math.random() * GAME_HEIGHT,
+			});
+		}
+	};
+
+	subscribe(world, LevelStart.type, handleLevelStart);
+
+	return {
+		execute: () => {
+			// do nothing
+		},
+	};
+};
