798fd1f281ede3a52a25be9b8f972334aa99f4cc
Chris Shepherd <chris@chrisshepherd.me>
2023-09-14 22:34:19 +0100

    Add events to World

--- src/ecs/world.ts
+++ src/ecs/world.ts
@@ -4,13 +4,17 @@ import type { System } from "./system";
 
 export type World = {
 	entities: Entity[];
+	eventEmitter: EventTarget;
 	lastId: number;
 	renderSystems: System[];
 	systems: System[];
 };
 
+export type WorldEventListener<TEvent extends Event> = (event: TEvent) => void;
+
 export const createWorld = (): World => ({
 	entities: [],
+	eventEmitter: new EventTarget(),
 	lastId: 0,
 	renderSystems: [],
 	systems: [],
@@ -50,4 +54,27 @@ export const render = (world: World, deltaTime: number): void => {
 	world.renderSystems.forEach((renderSystem) =>
 		renderSystem.execute(world.entities, deltaTime),
 	);
+};
+
+export const subscribe = <TEvent extends Event>(
+	world: World,
+	type: TEvent["type"],
+	listener: WorldEventListener<TEvent>,
+): void => {
+	world.eventEmitter.addEventListener(type, listener as EventListener);
+};
+
+export const unsubscribe = <TEvent extends Event>(
+	world: World,
+	type: TEvent["type"],
+	listener: WorldEventListener<TEvent>,
+): void => {
+	world.eventEmitter.removeEventListener(type, listener as EventListener);
+};
+
+export const dispatchEvent = <TEvent extends Event>(
+	world: World,
+	event: TEvent,
+): void => {
+	world.eventEmitter.dispatchEvent(event);
 };
