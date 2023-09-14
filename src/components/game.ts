5c3411d80b56536cd84828dfab91ffd53c2751ee
Chris Shepherd <chris@chrisshepherd.me>
2023-09-14 22:25:52 +0100

    Add Game component

---
+++ src/components/game.ts
@@ -0,0 +1,21 @@
+import type { Component } from "../ecs/component";
+
+export const GameType = "GAME";
+
+export type Game = {
+	hasStarted: boolean;
+	level: number;
+	lives: number;
+	score: number;
+	totalTime: number;
+	type: typeof GameType;
+} & Component;
+
+export const createGame = (): Game => ({
+	hasStarted: false,
+	level: 1,
+	lives: 3,
+	score: 0,
+	totalTime: 0,
+	type: GameType,
+});
