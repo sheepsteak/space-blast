55abefd3e6414ff2cf5d69babb1c0577e32454c8
Chris Shepherd <chris@chrisshepherd.me>
2023-10-10 12:11:38 +0100

    Add getRandomNumber function

--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,6 @@
 export const toDegrees = (radians: number): number => radians * (180 / Math.PI);
 
 export const toRadians = (degrees: number): number => degrees * (Math.PI / 180);
+
+export const getRandomNumber = (min: number, max: number): number =>
+	Math.random() * (max - min) + min;
