4adf9e0bd8700179623c5f82633c06d52c731936
Chris Shepherd <chris@chrisshepherd.me>
2023-12-06 21:15:58 +0000

    Refactor game start event handling

--- src/states/play/systems/game.ts
+++ src/states/play/systems/game.ts
@@ -10,22 +10,24 @@ import {
 } from "../../../ecs/world";
 import { GameStartEvent, LevelStartEvent } from "../events";
 
+const getGameComponent = (world: World): Game => {
+	const gameEntity = world.entities.find((entity) =>
+		hasEntityComponent(entity, GameType),
+	);
+
+	if (!gameEntity) {
+		throw new Error("Game entity not found");
+	}
+	return getEntityComponent<Game>(gameEntity, GameType);
+};
+
 export type CreateGameSystemArgs = {
 	world: World;
 };
 
 export const createGameSystem = ({ world }: CreateGameSystemArgs): System => {
 	const handleGameStart: WorldEventListener<GameStartEvent> = () => {
-		const gameEntity = world.entities.find((entity) =>
-			hasEntityComponent(entity, GameType),
-		);
-
-		if (!gameEntity) {
-			return;
-		}
-
-		const game = getEntityComponent<Game>(gameEntity, GameType);
-
+		const game = getGameComponent(world);
 		game.hasStarted = true;
 		game.level = 1;
 
